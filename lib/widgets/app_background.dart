0e055de67d8c670fee7381f16cedef2cadf7ef45
m.salman <m.salman@tpsonline.com>
2022-05-16 18:42:32 +0500

    Update app_background.dart
    first commit

--- lib/widgets/app_background.dart
+++ lib/widgets/app_background.dart
@@ -11,17 +11,35 @@ class AppBackground extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(color: kBlack),
-        Image.asset(
-          ImageDir.BG_LANDING,
-          fit: BoxFit.fitWidth,
-          width: double.infinity,
+        Positioned(
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(color: kBlack),
         ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
+        Positioned(
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
           child: Image.asset(
-            ImageDir.MAPS_DOTS,
-            fit: BoxFit.contain,
+            ImageDir.BG_LANDING,
+            fit: BoxFit.fitWidth,
+            width: double.infinity,
+          ),
+        ),
+        Positioned(
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset(
+              ImageDir.MAPS_DOTS,
+              fit: BoxFit.contain,
+            ),
           ),
         ),
         Positioned(
